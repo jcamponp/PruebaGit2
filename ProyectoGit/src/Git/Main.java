20e7efa58eac3715e0a23184e1475938c05e0df3
Maikel <Maikel@Ivan>
2026-05-18 19:56:56 +0200

    Soy el segundo commit

--- ProyectoGit/src/Git/Main.java
+++ ProyectoGit/src/Git/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Mi primer commit");
 		System.out.println("Soy Nadia Mejdoubi");
+		System.out.println("SOy Miguel Angel");
 	}
 }
