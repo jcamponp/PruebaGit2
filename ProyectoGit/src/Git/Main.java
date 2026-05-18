dc2bca6e4dfe1d98f0cbd82b1a75d7cd01cbebbd
mustapha <dam1mustapha.hm@gmail.com>
2026-05-18 19:36:24 +0200

    mi commit

--- ProyectoGit/src/Git/Main.java
+++ ProyectoGit/src/Git/Main.java
@@ -3,5 +3,6 @@ package Git;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Mi primer commit");
+		System.out.println(" Soy el quinto commit y mi nombre es el mustapha");
 	}
 }
